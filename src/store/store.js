82827c8bcb43ecf45e2d3f7764cb84479ad9630e
Catalina Gherzan <Catalina.Gherzan@toluna.com>
2019-11-22 17:52:22 +0200

    adding redux dev tools

--- src/store/store.js
+++ src/store/store.js
@@ -1,7 +1,8 @@
 import { createStore, applyMiddleware } from "redux";
 import asyncReducer from "./reducers";
 import thunk from "redux-thunk";
+import {composeWithDevTools} from "redux-devtools";
 
-const store =createStore(asyncReducer,applyMiddleware(thunk));
+const store =createStore(asyncReducer,applyMiddleware(thunk),composeWithDevTools);
 
 export default store;
