3ff08c304d893ac3dcd74cd23d84794be17e9ebc
Catalina Gherzan <Catalina.Gherzan@toluna.com>
2019-11-22 18:02:07 +0200

    adding thunk action

--- src/App.js
+++ src/App.js
@@ -1,14 +1,26 @@
 import React from 'react';
 import { connect } from "react-redux";
+import { thunkActionCreator } from "./store/actions/fetchAction";
 import Quote from "./components/Quote";
 import './App.css';
 
 class App extends React.Component {
+  getQuote = () => {
+    this.props.dispatch(thunkActionCreator);
+  }
+
   render(){
     return (
       <div className="App">
-        <button>Generate an SMARTASS programming quote</button>
-        <Quote quote={this.props.quote} />
+        <button onClick={this.getQuote}>Generate an SMARTASS programming quote</button>
+        {this.props.data.isFetching ? <h3>Loading...</h3> : null}
+        {this.props.data.isError ? (
+          <h3 className="error">No quote. Try again!</h3>
+        ) : null}
+        {this.props.data.quote.length > 0 ? (
+          <Quote quote={this.props.data.quote} />
+        ) : null}
+        
       </div>
     )
   }
