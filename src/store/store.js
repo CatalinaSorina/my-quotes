f0e454cf08f5e6bbc426ea640f60e92cf2ccd6da
Catalina <gherzan.catalina@yahoo.com>
2019-11-23 20:45:24 +0200

    create store with enhancer for dev-tools & thunk

--- src/store/store.js
+++ src/store/store.js
@@ -1,8 +1,13 @@
-import { createStore, applyMiddleware } from "redux";
+import { createStore, applyMiddleware, compose } from "redux";
 import asyncReducer from "./reducers";
-import thunk from "redux-thunk";
-import {composeWithDevTools} from "redux-devtools";
+import reduxThunk from "redux-thunk";
 
-const store =createStore(asyncReducer,applyMiddleware(thunk),composeWithDevTools);
+const composeEnhancers =
+  typeof window === "object" && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+    ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({})
+    : compose;
+const enhancer = composeEnhancers(applyMiddleware(reduxThunk));
+
+const store = createStore(asyncReducer, enhancer);
 
 export default store;
