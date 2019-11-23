5d60d56d03f742367093931794dfabf0734c0378
Catalina <gherzan.catalina@yahoo.com>
2019-11-23 22:55:35 +0200

    added async fetch, cleaning code & added redux hooks

--- src/App.js
+++ src/App.js
@@ -1,35 +1,28 @@
-import React from 'react';
-import { connect } from "react-redux";
+import React from "react";
+import { useSelector, useDispatch } from "react-redux";
 import { thunkActionCreator } from "./store/actions/fetchAction";
 import Quote from "./components/Quote";
-import './App.css';
+import "./App.css";
 
-class App extends React.Component {
-  getQuote = () => {
-    this.props.dispatch(thunkActionCreator);
-  }
+function App() {
+  const data = useSelector(state => state);
+  const dispatch = useDispatch();
 
-  render(){
-    return (
-      <div className="App">
-        <button onClick={this.getQuote}>Generate an SMARTASS programming quote</button>
-        {this.props.data.isFetching ? <h3>Loading...</h3> : null}
-        {this.props.data.isError ? (
-          <h3 className="error">No quote. Try again!</h3>
-        ) : null}
-        {this.props.data.quote.length > 0 ? (
-          <Quote quote={this.props.data.quote} />
-        ) : null}
-        
-      </div>
-    )
-  }
+  return (
+    <div className="App">
+      <button
+        className="App-button"
+        onClick={() => dispatch(thunkActionCreator())}
+      >
+        Generate an SMARTASS programming quote
+      </button>
+      {data.isFetching && <h3>Loading...</h3>}
+      {data.isError && data.quote && (
+        <h3 className="App-error">{data.quote}</h3>
+      )}
+      {data.quote.length > 0 && !data.isError && <Quote quote={data.quote} />}
+    </div>
+  );
 }
 
-const mapStateToProps = state => {
-  return {
-    data: state
-  };
-};
-
-export default connect(mapStateToProps)(App);
+export default App;
--- src/store/actions/fetchAction.js
+++ src/store/actions/fetchAction.js
@@ -1,37 +1,48 @@
-import store from "../store";
 import * as actionTypes from "./actionTypes";
 
 export const fetchQuote = () => {
-    return {
-        type: actionTypes.FETCH_QUOTE
-    };
+  return {
+    type: actionTypes.FETCH_QUOTE
+  };
 };
 
 export const receiveQuote = quote => {
-    return {
-        type: actionTypes.FETCHED_QUOTE,
-        data: quote
-    };
+  return {
+    type: actionTypes.FETCHED_QUOTE,
+    data: quote
+  };
 };
 
-export const errorMessage = () => {
-    return {
-        type: actionTypes.ERROR_MESSAGE
-    };
+export const errorMessage = msg => {
+  return {
+    type: actionTypes.ERROR_MESSAGE,
+    data: msg
+  };
 };
 
-export const thunkActionCreator = () => {
-    store.dispatch(fetchQuote());
-    return function(dispatch){
-        return fetch("https://programming-quotes-api.herokuapp.com/quotes/random")
-            .then(data => data.json())
-            .then(data => {
-                if(!data.en) {
-                    throw new Error("No quote now. Try again.")
-                }else{
-                    dispatch(receiveQuote(data.en));
-                }
-            })
-            .catch(error=>dispatch(errorMessage()));
-    }
-}
+export const thunkActionCreator = () => dispatch => {
+  dispatch(fetchQuote());
+  fetch("https://programming-quotes-api.herokuapp.com/quotes/random")
+    .then(data => data.json())
+    .then(data => {
+      return !data.en
+        ? dispatch(errorMessage("Wrong data !"))
+        : dispatch(receiveQuote(data.en));
+    })
+    .catch(error => dispatch(errorMessage()));
+};
+
+// export const thunkActionCreator = () => async dispatch => {
+//   dispatch(fetchQuote());
+//   const res = await fetch(
+//     "https://programming-quotes-api.herokuapp.com/quotes/random"
+//   );
+//   let data = await res.json();
+//   if (!data.en) {
+//     dispatch(errorMessage());
+//   } else {
+//     data.en
+//       ? dispatch(receiveQuote(data.en))
+//       : dispatch(receiveQuote("Wrong data !"));
+//   }
+// };
