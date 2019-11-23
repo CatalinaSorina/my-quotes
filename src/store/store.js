c24864eadef695a4ce97a69230a259635dea45a7
Catalina <gherzan.catalina@yahoo.com>
2019-11-23 21:07:58 +0200

    lighter version of composeEnhancers

--- src/store/store.js
+++ src/store/store.js
@@ -2,10 +2,7 @@ import { createStore, applyMiddleware, compose } from "redux";
 import asyncReducer from "./reducers";
 import reduxThunk from "redux-thunk";
 
-const composeEnhancers =
-  typeof window === "object" && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
-    ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({})
-    : compose;
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const enhancer = composeEnhancers(applyMiddleware(reduxThunk));
 
 const store = createStore(asyncReducer, enhancer);
